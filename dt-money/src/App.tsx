f19f82963810daa790bd27daa4c2fe61b6eee6d3
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-11-18 19:48:43 -0300

    Componente: NewTransactionModal

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -24,12 +25,10 @@ export function App() {
             <Header onOpenNewTransactionModal={handleOpenNewTransactionsModal} />
             <Dashboard />
 
-            <Modal
+            <NewTransactionModal 
                 isOpen={isNewTransactionModalOpen}
                 onRequestClose={handleCloseNewTransactionsModal}
-            >
-                <h2>Casdastrar</h2>
-            </Modal>
+            />
             <GlobalStyle />
         </>
     );
