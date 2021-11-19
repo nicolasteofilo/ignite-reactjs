fdf5fe3eda6a90514c5b3856d6cf2c60349b1ccd
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-11-19 20:53:39 -0300

    Cores dos botões

--- dt-money/src/components/NewTransactionModal/index.tsx
+++ dt-money/src/components/NewTransactionModal/index.tsx
@@ -41,6 +41,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="button"
                     onClick={() => setType('deposit')}
                     isActive={type === 'deposit'}
+                    activeColor="green"
                 >
                     <img src={incomeImg} alt="Entrada" />
                     <span>Entrada</span>
@@ -49,6 +50,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="button"
                     onClick={() => setType('withdraw')}
                     isActive={type === 'withdraw'}
+                    activeColor="red"
                 >
                     <img src={outcomeImg} alt="Entrada" />
                     <span>Saída</span>
