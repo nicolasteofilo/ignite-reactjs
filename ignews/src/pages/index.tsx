f3a1756e3de6021d3e4ac8c83acccddf9d3425a6
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-11-29 09:27:27 -0300

    fix: home page [ ig.news ]

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Home | ig.news</title>
-      </Head>
-      <h1>Hello Word</h1>  
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Home | ig.news</title>
+            </Head>
+            <main className={styles.contentConteiner} >
+                <section className={styles.hero}>
+                    <span>👏 Olá, seja bem vindo</span>
+                    <h1>Notícias sobre o universo <span>React</span></h1>
+                    <p>
+                        Garanta seu acesso a todas as publicações
+                        <br />
+                        <span>por R$9.90 mensal</span>
+                    </p>
+
+                </section>
+
+                <img src="/images/avatar.svg" alt="Mulher Programando" />
+            </main>
+        </>
+    )
 }
