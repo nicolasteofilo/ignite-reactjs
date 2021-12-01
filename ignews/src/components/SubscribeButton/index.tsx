37ac00c0e4aea8baf97e8d7022753ce6aa024895
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-11-30 21:14:13 -0300

    fix: consuming stripe api with SSR [ ig.news ]

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,12 +1,13 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
-   return (
-       <button
-            type="button"
-            className={styles.subscribeButton}
-       >
-           Inscreva-se agora
-       </button>
-   ) 
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    return (
+        <button type="button" className={styles.subscribeButton}>
+            Inscreva-se agora
+        </button>
+    );
 }
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,28 +1,62 @@
-import Head from 'next/head';
-import { SubscribeButton } from '../components/SubscribeButton';
+import {} from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-import styles from './home.module.scss';
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+    product: {
+        productId: string;
+        amount: number;
+    };
+}
+
+export default function Home({ product }: HomeProps) {
     return (
         <>
             <Head>
                 <title>Home | ig.news</title>
             </Head>
-            <main className={styles.contentConteiner} >
+            <main className={styles.contentConteiner}>
                 <section className={styles.hero}>
                     <span>👏 Olá, seja bem vindo</span>
-                    <h1>Notícias sobre o universo <span>React</span></h1>
+                    <h1>
+                        Notícias sobre o universo <span>React</span>
+                    </h1>
                     <p>
                         Garanta seu acesso a todas as publicações
                         <br />
-                        <span>por R$9.90 mensal</span>
+                        <span>por {product?.amount} mensal</span>
                     </p>
-                    <SubscribeButton />
+                    <SubscribeButton priceId={product.productId} />
                 </section>
 
                 <img src="/images/avatar.svg" alt="Mulher Programando" />
             </main>
         </>
-    )
+    );
 }
+
+export const getStaticProps = async () => {
+    const price = await stripe.prices.retrieve(
+        "price_1JnBoNJyqcNOjjEN127FGdWq",
+        {
+            expand: ["product"],
+        }
+    );
+
+    const product = {
+        id: price.id,
+        amount: new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        }).format(price.unit_amount / 100),
+    };
+
+    return {
+        props: {
+            product,
+        },
+    };
+};
