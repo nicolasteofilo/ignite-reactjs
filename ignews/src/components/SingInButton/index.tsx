1fafd2c43783be1ac87702271e7415ef644f1cd6
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-12-02 10:23:26 -0300

    fix: Sing in with github

--- ignews/src/components/SingInButton/index.tsx
+++ ignews/src/components/SingInButton/index.tsx
@@ -1,24 +1,32 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 import styles from './styles.module.scss';
 
 export function SingInButton() {
-    const isuserLoggedIn = true;
+    const [session] = useSession()
 
-    return isuserLoggedIn ? (
-        <button
-            type="button"
-            className={styles.singInButton}
-        >
-            <FaGithub color="#04d361" />
-            Nicolas Teófilo
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
+    console.log(session)
+
+
+    return session ? (
+        <>
+            <button
+                type="button"
+                className={styles.singInButton}
+            >
+                <FaGithub color="#04d361" />
+                {session?.user?.name}
+                <FiX onClick={() => signOut()} color="#737380" className={styles.closeIcon} />
+            </button>
+            <img className={styles.imageAvatar} src={session?.user?.image} alt="" />
+        </>
     ) : (
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sing in with GitHub
