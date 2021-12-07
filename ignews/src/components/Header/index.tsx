33f6ddea1220f9db0da869db61efc5039f3c840d
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-12-07 12:52:45 -0300

    fix: listening to stripe webhooks [ ignews ]

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { SingInButton } from '../SingInButton';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
 export function Header() {
+    function handleClickImageLogo() {
+        window.location.href = '/';
+    }
+
     return (
         <header className={styles.headerContainer} >
             <div className={styles.headerContent} >
-                <img src="/images/logo.svg" alt="ig.news" />
+                <Link href="/">
+                 <img src="/images/logo.svg" alt="ig.news" />
+                </Link>
                 <nav>
                     <a className={styles.active} href="#">Home</a>
                     <a href="#">Posts</a>
