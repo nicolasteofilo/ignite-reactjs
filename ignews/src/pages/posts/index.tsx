f3f2f47548d1fae17e37a42c7744ad5b60fdac39
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-12-17 22:00:43 -0300

    fix: page post [ ignews ]

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-key */
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
+import Link from 'next/link';
 
 import { getPrismicClient } from "../../services/prismic";
 
@@ -28,11 +30,13 @@ export default function Posts({ posts }: PostsProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map((post) => (
-                        <a href="#" key={post.slug}>
+                      <Link href={`/posts/${post.slug}`}>
+                        <a key={post.slug}>
                             <time>{post.updatedAt}</time>
                             <strong>{post.title}</strong>
                             <p>{post.excerpt}</p>
                         </a>
+                      </Link>
                     ))}
                 </div>
             </main>
