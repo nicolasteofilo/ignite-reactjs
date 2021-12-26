92112b496df7bd0510aa9697b4af818700cb9ca8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-12-25 22:30:17 -0300

    fix: create sidebar components [ spacetraveling ]

---
+++ dashgo/src/components/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+
+export default function Sidebar() {
+  return (
+    <Box as="aside" w="64">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="sm">
+            GERAL
+            <Stack spacing="4" mt="8" align="stretch">
+              <Link href="#" display="flex" align="center">
+                <Icon as={RiDashboardLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Dashboard</Text>
+              </Link>
+              <Link href="#" display="flex" align="center">
+                <Icon as={RiContactsLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Úsuarios</Text>
+              </Link>
+            </Stack>
+          </Text>
+        </Box>
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="sm">
+            AUTOMAÇÂO
+            <Stack spacing="4" mt="8" align="stretch">
+              <Link href="#" display="flex" align="center">
+                <Icon as={RiInputMethodLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Forms</Text>
+              </Link>
+              <Link href="#" display="flex" align="center">
+                <Icon as={RiGitMergeLine} fontSize="20" />
+                <Text ml="4" fontWeight="medium">Automação</Text>
+              </Link>
+            </Stack>
+          </Text>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
--- dashgo/src/pages/dashboard.tsx
+++ dashgo/src/pages/dashboard.tsx
@@ -1,12 +1,13 @@
 import { Flex } from "@chakra-ui/react";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
-      <Flex width="100%" my="6" maxWidth={1480}>
-
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
       </Flex>
     </Flex>
   )
