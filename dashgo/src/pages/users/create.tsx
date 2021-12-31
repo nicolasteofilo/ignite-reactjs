43cab1f3649b9c27c27cb2a338a65a618e6d4d62
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-12-31 17:08:32 -0300

    fix: validation form create user

--- dashgo/src/pages/users/create.tsx
+++ dashgo/src/pages/users/create.tsx
@@ -1,17 +1,60 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
 
 import Header from "../../components/Header";
 import { Input } from "../../components/Form/Input";
 import Sidebar from "../../components/Sidebar";
 import Link from "next/link";
 
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const createUserFormShema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().required("E-mail obrigatória"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "Senha deve ter no mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .required("Confirmação de senha obrigatória")
+    .oneOf([null, yup.ref("password")], "Senhas não conferem"),
+});
+
 export default function CreateUser() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(createUserFormShema),
+  });
+
   const date = new Date();
-  const dateFomated = Intl.DateTimeFormat("pt-BR", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  }).format(date);
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(values);
+  };
 
   return (
     <Box>
@@ -20,30 +63,65 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bgColor="gray.800" p={["6","8"]}>
-          <Heading size="lg" fontWeight="normal">Criar Úsuarios</Heading>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bgColor="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
+          <Heading size="lg" fontWeight="normal">
+            Criar Úsuarios
+          </Heading>
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" type="email" label="E-mail" />
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="name"
+                label="Nome completo"
+                type="text"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register("email")}
+              />
             </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing={["6","8"]} w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmaçao de Senha" />
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                error={errors.password}
+                {...register("password")}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmaçao de Senha"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
+              />
             </SimpleGrid>
           </VStack>
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
               <Link href="/users" passHref>
-                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button type="submit" isLoading={isSubmitting} colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
         </Box>
       </Flex>
     </Box>
-  )
+  );
 }
