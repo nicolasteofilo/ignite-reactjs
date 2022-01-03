ed5434caf5d4a038dc1815fdd80288ed9af744d5
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-03 15:10:09 -0300

    fix: aumentando quantidade [ redux ]

--- bonus/redux/src/store/modules/cart/reducer.ts
+++ bonus/redux/src/store/modules/cart/reducer.ts
@@ -11,7 +11,17 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
     switch (action.type) {
       case "ADD_PRODUCT_TO_CART": {
         const { product } = action.payload;
-        draft.items.push({ product, quantity: 1 });
+
+        const productIndex = draft.items.findIndex(item => 
+          item.product.id === product.id,
+        );
+
+        if (productIndex >= 0) {
+          draft.items[productIndex].quantity += 1;
+        } else {
+          draft.items.push({ product, quantity: 1 });
+        }
+
         break;
       }
       default: {
