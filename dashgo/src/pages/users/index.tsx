7146706c95bcba34f0596af70564a5f96d02db4e
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-06 10:25:48 -0300

    fix: Setting react-query

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -21,8 +22,15 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
+import { useQuery } from "react-query";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -34,12 +42,6 @@ export default function UserList() {
     month: "short",
     day: "numeric",
   }).format(date);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/users")
-    .then(res => res.json())
-    .then(data => console.log(data))
-  })
 
   return (
     <Box>
@@ -69,55 +71,66 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Úsuario</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Nicolas Teófilo</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      nicolas@email.com.br
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <Td px="6">
-                    <Text>{dateFomated}</Text>
-                  </Td>
-                )}
-                {isWideVersion ? (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="15"
-                      colorScheme="purple"
-                      backgroundColor="gray.600"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                      cursor={"pointer"}
-                    >
-                      {isWideVersion ? "Editar" : null}
-                    </Button>
-                </Td>
-                ) : null }
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Falha ao obter dados!</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Úsuario</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th w="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Nicolas Teófilo</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          nicolas@email.com.br
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && (
+                      <Td px="6">
+                        <Text>{dateFomated}</Text>
+                      </Td>
+                    )}
+                    {isWideVersion ? (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          fontSize="15"
+                          colorScheme="purple"
+                          backgroundColor="gray.600"
+                          leftIcon={<Icon as={RiPencilLine} />}
+                          cursor={"pointer"}
+                        >
+                          {isWideVersion ? "Editar" : null}
+                        </Button>
+                      </Td>
+                    ) : null}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
