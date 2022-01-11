037fb18beaafeb29d0fe2c2f5448d88e75f7161c
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-11 18:17:03 -0300

    fix: pagination component

--- dashgo/src/components/Pagination/index.tsx
+++ dashgo/src/components/Pagination/index.tsx
@@ -1,7 +1,50 @@
-import { Box, Button, HStack, Stack } from "@chakra-ui/react";
+import { Box, Button, HStack, Stack, Text } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
+import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.ceil(totalCountOfRegisters / registersPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
+  console.log({
+    previousPages,
+    nextPages,
+  });
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -12,23 +55,50 @@ export function Pagination() {
     >
       <Box>
         <strong>0</strong>
-        {' - '}
+        {" - "}
         <strong>10</strong>
-        {' de '}
-        <strong>
-          100
-        </strong>
+        {" de "}
+        <strong>100</strong>
       </Box>
-      <HStack
-        spacing="2"
-      >
-        <PaginationItem number={1} isCurrent={true} />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
-        <PaginationItem number={6} />
-        <PaginationItem number={7} />
+      <HStack spacing="2">
+        {/* Fist Pages */}
+        {currentPage > 1 + siblingsCount && (
+          <>
+            <PaginationItem number={1} />
+            {currentPage > 2 + siblingsCount && (
+              <Text
+                color="gray.300"
+                width="8"
+                textAlign="center"
+              >...</Text>
+            )}
+          </>
+        )}
+        {/* Previuos Page */}
+        {previousPages.length > 0 &&
+          previousPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+        <PaginationItem number={currentPage} isCurrent={true} />{" "}
+        {/* current page */}
+        {/* Next Pages */}
+        {nextPages.length > 0 &&
+          nextPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+        {currentPage + siblingsCount < lastPage && (
+          <>
+            {(currentPage + 1 + siblingsCount) < lastPage && (
+              <Text
+                color="gray.300"
+                width="8"
+                textAlign="center"
+              >...</Text>
+            )}
+            <PaginationItem number={lastPage} />
+          </>
+        )}
       </HStack>
     </Stack>
-  )
+  );
 }
--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -122,7 +122,11 @@ export default function UserList() {
                   })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={200}
+                currentPage={10}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
