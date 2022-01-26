0fe8ed0341e61f187e9500d3717c23d06a7a5da8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-01-25 21:43:38 -0300

    feat: refresh token

--- autentication/auth-with-jwt/services/api.ts
+++ autentication/auth-with-jwt/services/api.ts
@@ -1,11 +1,45 @@
-import axios from 'axios';
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from "axios";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies()
+let cookies = parseCookies();
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    headers: {
-        Authorization: `Bearer ${cookies['@nexauth.refreshToken']}`
-    }
+  baseURL: "http://localhost:3333",
+  headers: {
+    Authorization: `Bearer ${cookies["@nexauth.refreshToken"]}`,
+  },
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response?.data?.code) {
+        cookies = parseCookies();
+
+        const { "@nexauth.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response?.data;
+            setCookie(undefined, "@nexauth.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 dias
+              path: "/",
+            });
+            setCookie(undefined, "@nexauth.refreshToken", response.data.refreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 dias
+              path: "/",
+            });
+
+            api.defaults.headers['Authorization'] = `Bearer ${token}`;
+          });
+      } else {
+      }
+    }
+  }
+);
